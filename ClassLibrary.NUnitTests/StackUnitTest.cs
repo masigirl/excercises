06cedab3dee27e25c2b28cbc4c29e71169b5a9e8
masigirl <machilok@gmail.com>
2018-08-06 21:16:09 -0600

    Adding NUnit tests

---
+++ ClassLibrary.NUnitTests/StackUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.NUnitTests
+{
+    [TestFixture]
+    public class StackUnitTest
+    {
+        [TestCase]
+        public void Push_NullObject_ThrowsException()
+        {
+            var Stack = new ClassLibrary.Stack();
+            Assert.Throws(typeof(InvalidOperationException), ()=> Stack.Push(null));
+        }
+
+        [TestCase]
+        public void Pop_EmptyStack_ThrowsException()
+        {
+            var Stack = new ClassLibrary.Stack();
+            Assert.Throws(typeof(InvalidOperationException), () => Stack.Pop());
+            
+        }
+    }
+}
